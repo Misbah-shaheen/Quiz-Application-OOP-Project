cff1a1a030eae9a25b9fbab812f1d3eea5035562
Misbah Shaheen <163327045+Misbah-shaheen@users.noreply.github.com>
2024-05-24 22:36:50 +0500

    Subjects.java

---
+++ Subjects.java
@@ -0,0 +1,11 @@
+//interface that is implemeted by all four subjects
+ public interface Subjects {
+ 
+    void initQuiz();
+    void setDate();
+    void setupTimer();
+    void displayStudentInfo();
+    void loadQuestion();
+    void answerCheck();
+    void submit();
+}
